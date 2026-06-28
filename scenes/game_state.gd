75bb085b5ae2a8df3f9dc15779a9b946547d54a9
Khelta <jonas-helm@gmx.de>
2026-06-28 21:02:36 +0200

    placement mode in game state

--- scenes/game_state.gd
+++ scenes/game_state.gd
@@ -1,10 +1,17 @@
 extends Node
 
+signal placement_mode_changed(is_on: bool)
+
+@export var is_placement_mode : bool = false:
+	set(value):
+		is_placement_mode = value
+		placement_mode_changed.emit(value)
+
+
+
 func _ready() -> void:
-	var path_length: float = get_child(0).curve.get_baked_length()
-	var enemy_speed = 20
-	var time_for_path = path_length * enemy_speed
-	var progress_ratio_speed = 100 / time_for_path
+	add_to_group("game_state")
+	print(get_tree().get_first_node_in_group("game_state"))
 
 
 func _process(delta):
--- scripts/tiles/tower_tile.gd
+++ scripts/tiles/tower_tile.gd
@@ -7,17 +7,43 @@ extends Node3D
 		set_material()
 @export var tower: Node3D
 
-
 func _ready() -> void:
 	set_material()
-
+	set_valid_tower_position_vfx_visibility(false)
+	connect_to_game_state()
+	
+		
+func connect_to_game_state():
+	await get_tree().process_frame
+	var game_state = get_tree().get_first_node_in_group("game_state")
+	if game_state:
+		game_state.placement_mode_changed.connect(_on_game_state_placement_mode_changed)
 
 func set_material() -> void:
 	const default_material = null
 	const non_placeable_material = preload("res://materials/invalid_placeable_material.tres")
-	var floor_mesh = get_child(0)
+	var floor_mesh = get_node("FloorDirt")
 	
 	if is_placeable:
 		floor_mesh.set_surface_override_material(0, default_material)
 	else:
 		floor_mesh.set_surface_override_material(0, non_placeable_material)
+
+
+func is_valid_placement() -> bool:
+	if is_placeable:
+		return true
+	return false
+
+
+func set_valid_tower_position_vfx_visibility(is_visible : bool) -> void:
+	var vfx = get_node("ValidTowerPositionVFX")
+	vfx.visible = is_visible
+
+
+func _on_game_state_placement_mode_changed(placement_mode_is_on: bool) -> void:
+	var validTowerPositionVFX = get_node("ValidTowerPositionVFX")
+	if placement_mode_is_on and is_valid_placement():
+		validTowerPositionVFX.visible = true
+	else:
+		validTowerPositionVFX.visible = false
