c7de5a6784b0520d2a325d8aaa8cb1a3655f1483
Khelta <jonas-helm@gmx.de>
2026-07-16 22:57:43 +0200

    Implemented holding input logic

--- scripts/selector.gd
+++ scripts/selector.gd
@@ -24,6 +24,11 @@ var grabbed_tower : BaseTower:
 			grabbed_tower._on_grabbed_changed(true)
 var grabbed_tower_slot : Slot
 
+var holding : bool = false
+var hold_completed : bool = false
+var hold_time : float = 0.3
+var hold_timer : float = 0.0
+
 @export var is_placement_mode : bool = false:
 	set(value):
 		is_placement_mode = value
@@ -37,9 +42,14 @@ func _ready() -> void:
 	connect_to_buttons()
 
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	if grabbed_tower:
 		grabbed_tower.global_position = MouseUtils.get_mouse_world_position()
+
+	if holding:
+		hold_timer += delta
+	if hold_timer >= hold_time:
+		hold_completed = true
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -60,16 +70,16 @@ func _unhandled_input(event: InputEvent) -> void:
 
 	if event.is_action_pressed("select"):
 		var slot = MouseUtils.select_entity_from_mouse(3)
+		hold_started()
+		tower_input(slot)
+
+
+	if event.is_action_released("select"):
+		var slot = MouseUtils.select_entity_from_mouse(3)
 		
-		if slot is BenchSlot:
-			if slot.tower:
-				grab_tower(slot)
-			elif slot.is_free():
-				place_grabbed_tower_in_slot(slot)
-				
-		if slot is TowerTile:
-			if grabbed_tower:
-				place_grabbed_tower_in_slot(slot)
+		if holding and hold_completed:
+			tower_input(slot)
+		reset_hold()
 
 
 func connect_to_buttons() -> void:
@@ -151,3 +161,27 @@ func set_ui_tower_preview(tower: BaseTower) -> void:
 
 func set_fresnel_selection(tower: BaseTower, is_on) -> void:
 	tower.get_node("Model")._set_selection(is_on)
+
+
+func hold_started() -> void:
+	holding = true
+	hold_timer = 0.0
+	hold_completed = false
+
+
+func reset_hold() -> void:
+	holding = false
+	hold_timer = 0.0
+	hold_completed = false
+
+
+func tower_input(slot: Slot):
+	if slot is BenchSlot:
+		if slot.tower:
+			grab_tower(slot)
+		elif slot.is_free():
+			place_grabbed_tower_in_slot(slot)
+				
+	if slot is TowerTile:
+		if grabbed_tower:
+			place_grabbed_tower_in_slot(slot)
