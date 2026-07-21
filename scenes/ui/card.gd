4464cb34054e4611b9b513402ad561320b073692
Khelta <jonas-helm@gmx.de>
2026-07-21 19:59:21 +0200

    Added card selection screen

---
+++ scenes/ui/card.gd
@@ -0,0 +1,17 @@
+extends AspectRatioContainer
+class_name Card
+
+var game_state : GameState
+@export var tower_name : String
+var button : TextureButton
+
+func _ready() -> void:
+	button = get_node("Card")
+	
+	await get_tree().process_frame
+	game_state = get_tree().get_first_node_in_group("game_state")
+
+
+func _on_card_button_down() -> void:
+	var towers : Towers = game_state.get_node("Towers")
+	towers.add_tower(tower_name)
---
+++ scenes/ui/card_selection.gd
@@ -0,0 +1,31 @@
+extends Control
+class_name CardSelection
+
+@export var  first_tower : String
+@export var second_tower : String
+@export var  third_tower : String
+
+var card_scene = preload("res://scenes/ui/card.tscn")
+
+func _ready() -> void:
+	var hbox = get_node("MarginContainer/HBoxContainer")
+	
+	var first_card = card_scene.instantiate()
+	first_card.tower_name = first_tower
+	
+	hbox.add_child(first_card)
+	first_card.button.button_down.connect(selected_card)
+	
+	var second_card = card_scene.instantiate()
+	second_card.tower_name = second_tower
+	hbox.add_child(second_card)
+	second_card.button.button_down.connect(selected_card)
+	
+	var third_card : Card = card_scene.instantiate()
+	third_card.tower_name = third_tower
+	hbox.add_child(third_card)
+	third_card.button.button_down.connect(selected_card)
+
+
+func selected_card() -> void:
+	queue_free()
