031e6853ba3579739d279e56798bb53e130f991e
Khelta <jonas-helm@gmx.de>
2026-07-22 23:03:57 +0200

    Added dynamic texture loading for card selection

--- scenes/ui/card.gd
+++ scenes/ui/card.gd
@@ -7,11 +7,23 @@ var game_state : GameState
 
 signal card_selected(card: Card)
 
+var ranger_image = preload("res://assets/preview_images/Ranger.png")
+var knight_image = preload("res://assets/preview_images/Knight.png")
+
+var tower_to_texture_dict = {
+	"Ranger": ranger_image,
+	"Knight": knight_image
+	}
+
 func _ready() -> void:
 	button = get_node("Card")
-	
+
 	await get_tree().process_frame
 	game_state = get_tree().get_first_node_in_group("game_state")
+
+	button.texture_normal = tower_to_texture_dict[
+		TowerUtils.remove_star_level_from_tower_string(tower_name)
+		]
 
 
 func _on_card_button_down() -> void:
