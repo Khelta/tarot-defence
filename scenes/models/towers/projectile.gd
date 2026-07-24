05d67cb0b0002865c0a8c71cb89fe274f87969ed
Khelta <jonas-helm@gmx.de>
2026-07-24 23:44:27 +0200

    Set curve local to scene to prevent it from being overwritten

--- scenes/models/towers/projectile.gd
+++ scenes/models/towers/projectile.gd
@@ -8,10 +8,10 @@ var last_progress_ratio : float = 0.0
 var enemy : BaseEnemy
 
 
-func spawn(tower: BaseTower, local_enemy: BaseEnemy, projectile_scene: PackedScene) -> void:
+func spawn(tower: BaseTower, local_enemy: BaseEnemy, projectile_mesh_scene: PackedScene) -> void:
 	enemy = local_enemy
 	
-	var projectile_instance = projectile_scene.instantiate()
+	var projectile_instance = projectile_mesh_scene.instantiate()
 	path_follow.add_child(projectile_instance)
 	
 	path.curve.add_point(path.to_local(tower.global_position))
@@ -21,7 +21,15 @@ func spawn(tower: BaseTower, local_enemy: BaseEnemy, projectile_scene: PackedSce
 func _process(delta: float) -> void:
 	print(path_follow.progress, " | ", path_follow.progress_ratio)
 	path_follow.progress += 0.05
+
+	# Update curve to enemy position
 	path.curve.set_point_position(1, path.to_local(enemy.global_position))
+
 	if path_follow.progress_ratio >= 1.0 or last_progress_ratio > path_follow.progress_ratio:
-		queue_free()
+		destroy()
+
 	last_progress_ratio = path_follow.progress_ratio
+
+
+func destroy() -> void:
+	queue_free()
