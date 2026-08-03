272d0960d7f8926121009bfc6711f1177b169816
Khelta <jonas-helm@gmx.de>
2026-08-03 21:15:24 +0200

    Added necromancer animations

--- scenes/enemies/variations/skeleton_necromancer_animation.gd
+++ scenes/enemies/variations/skeleton_necromancer_animation.gd
@@ -1,2 +1,14 @@
 class_name SkeletonNecromancerAnimation
 extends BasicEnemyAnimation
+
+func _ready() -> void:
+	super._ready()
+	var utils = AnimationUtils.new(animation_player)
+	utils.set_animation_on_loop("Rig_Medium_CombatRanged/Ranged_Magic_Spellcasting")
+
+func _casting() -> void:
+	animation_player.play("Rig_Medium_CombatRanged/Ranged_Magic_Spellcasting")
+
+
+func _on_cast_finished() -> void:
+	animation_player.play("Rig_Medium_CombatRanged/Ranged_Magic_Raise")
