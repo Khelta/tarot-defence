7bd67f2690bfedf4eea07ace69c741acac87f749
Jonas Helm <40138544+Khelta@users.noreply.github.com>
2026-08-19 22:57:12 +0200

    Corrected selector dependency

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -4,6 +4,8 @@ class_name UI
 @export var info_panel : InfoPanel
 @export var start_wave_button : StartWaveButton
 @export var end_screen : EndScreen
+@export var sell_tower_button : SellTowerButton
+@export var tower_panel : TowerPanel
 
 func _ready() -> void:
 	add_to_group("ui")
