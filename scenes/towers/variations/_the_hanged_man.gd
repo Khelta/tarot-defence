a447a4cc2df08b3f89063b2bd78f0141adaed6f1
Jonas Helm <40138544+Khelta@users.noreply.github.com>
2026-08-20 23:37:09 +0200

    Added tower scene and script for hanged man

---
+++ scenes/towers/variations/_the_hanged_man.gd
@@ -0,0 +1,12 @@
+@tool
+extends BaseTower
+class_name TheHangedMan
+
+
+func _ready() -> void:
+	super()
+
+"""
+	var hanged_man_animation : TheHangedManAnimation = get_node("Model")
+	self.attacked.connect(hanged_man_animation._on_base_tower_attack)
+"""
