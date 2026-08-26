bfcc65c689ab2e7f1e71f8aae61b0439c2b11d68
Jonas Helm <40138544+Khelta@users.noreply.github.com>
2026-08-26 20:09:27 +0200

    fixed wrong script init for slow and weakend effect

--- scenes/enemies/status_effects/slow.gd
+++ scenes/enemies/status_effects/slow.gd
@@ -3,7 +3,7 @@ class_name Slow
 
 var factor : float = 1.0
 
-func _init(target: BaseEnemy, slow_factor: float = 0.5, _base_duration: float = 3.0) -> void:
+func _init(target: BaseEnemy, effect_definition: EnemyStatusEffectDefinition, slow_factor: float = 0.5) -> void:
 	factor = slow_factor
 
-	super._init(target)
+	super._init(target, effect_definition)
--- scenes/enemies/status_effects/weakend.gd
+++ scenes/enemies/status_effects/weakend.gd
@@ -3,7 +3,7 @@ class_name Weakend
 
 var factor : float = 1.0
 
-func _init(target: BaseEnemy, weakend_factor: float = 1.25) -> void:
+func _init(target: BaseEnemy, effect_definition: EnemyStatusEffectDefinition, weakend_factor: float = 1.25) -> void:
 	factor = weakend_factor
 
-	super._init(target)
+	super._init(target, effect_definition)
