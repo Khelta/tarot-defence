02ea1a05264cafc60f3eab4343cb71b736608ab8
Jonas Helm <40138544+Khelta@users.noreply.github.com>
2026-08-28 12:17:36 +0200

    Tower data holds arcana number

--- scenes/towers/resources/tower_data.gd
+++ scenes/towers/resources/tower_data.gd
@@ -2,7 +2,12 @@ extends Resource
 class_name TowerData
 
 @export var name: String
+
 @export var tower_scene: PackedScene
+
+@export var is_great_arcana : bool = false
+@export var arcana_number : int = -1
+
 @export var rarity: int = 1
 @export var weight: float = 1.0
 @export var cost: int
